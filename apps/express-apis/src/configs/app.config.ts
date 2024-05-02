b81438d3c05061b7ea01583c19b3bba08d5e7d05
kartikw3dev <kartik.g@w3dev.email>
2024-05-02 11:50:15 +0530

    Database connection string changes

--- apps/express-apis/src/configs/app.config.ts
+++ apps/express-apis/src/configs/app.config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const DATABASE_CONFIGS = {
-  CONNECT_URI: process.env.DATABASE_URL || "mongodb+srv://kartikgupta962004:kartik96@discord-bot-cluster.cm6ulb5.mongodb.net/flyweis-asses-apis"
+  CONNECT_URI: process.env.DATABASE_URL || "mongodb+srv://kartikgupta962004:kartik96@discord-bot-cluster.cm6ulb5.mongodb.net/xicom-asses-apis"
 }
 
 export {
